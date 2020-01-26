7a78a3f9d338e74203358993eea7d4a5fa5d1002
gaben <gaben@glados.localdomain>
2020-01-26 20:40:52 +0300

    heat.cpp fixed p

--- heat.cpp
+++ heat.cpp
@@ -6,36 +6,22 @@ int main()
     constexpr size_t WIDTH = 800;
     constexpr size_t HEIGHT = 600;
     sf::RenderWindow window(sf::VideoMode(WIDTH,HEIGHT), "Thermal conductivity");
-    double **u = new double*[WIDTH];
-    double **ubuf = new double*[WIDTH]; 
+    double **ux = new double*[WIDTH];
+    double **uy = new double*[WIDTH];
+    double **uxbuf = new double*[WIDTH];
+    double **uybuf = new double*[WIDTH]; 
     for(size_t i = 0; i < WIDTH; i++){
-        u[i] = new double[HEIGHT];
-        ubuf[i] = new double[HEIGHT];
-    }
-    for (size_t i = 0; i < WIDTH; i++){
-        for (size_t j = 0; j < HEIGHT; j++){
-            u[i][j] = 0;
-            ubuf[i][j] =0;
-        }
+        ux[i] = new double[HEIGHT];
+        uxbuf[i] = new double[HEIGHT];
+        uy[i] = new double[HEIGHT];
+        uybuf[i] = new double[HEIGHT];
     }
     srand(228);
-    for (size_t i = 0; i < WIDTH - 1; i++){
-        for (size_t j = 0; j < HEIGHT -1  ; j++){
-            u[i][j] = (double) rand()/RAND_MAX;
-        }
-    }
-    for (size_t i = 50; i < 100; i++){
-        for (size_t j = 50; j < 100 ; j++){
-            u[i][j] = 0;
-        }
-    }
     for (size_t i = 0; i < WIDTH; i++){
-        u[i][0] = 0;
-        u[i][HEIGHT - 1] =0;
-    }
-    for (size_t j = 0; j < HEIGHT; j++){
-        u[0][j] =0;
-        u[WIDTH - 1][j] = 0;
+        for (size_t j = 0; j < HEIGHT; j++){
+            ux[i][j] = (double) rand()/RAND_MAX  * 0.5;
+            uy[i][j] = (double) rand()/RAND_MAX * 0.5 ;
+        }
     }
     sf::Uint8 pixels[WIDTH * HEIGHT * 4] = {};
     for (size_t i = 0; i < WIDTH; i++){
@@ -50,16 +36,33 @@ int main()
     while (window.isOpen()){
         for (size_t i = 1; i < WIDTH - 1; i++){
             for (size_t j = 1; j < HEIGHT - 1; j++){
-                double ddx = (u[i + 1][j] - 2 * u[i][j] + u[i - 1][j])/ 4;
-                double ddy = (u[i][j + 1] - 2 * u[i][j] + u[i][j - 1])/ 4;
+                double ddx = (ux[i + 1][j] - 2 * ux[i][j] + ux[i - 1][j])/ 4;
+                double ddy = (ux[i][j + 1] - 2 * ux[i][j] + ux[i][j - 1])/ 4;
                 double dt = 0.1* (ddx + ddy);
-                ubuf[i][j] = u[i][j] +  dt;
-                pixels[4 *(WIDTH *j + i)] = (uint8_t)(ubuf[i][j] * 255);
+                uxbuf[i][j] = ux[i][j] +  dt;
+                ddx = (uy[i + 1][j] - 2 * uy[i][j] + uy[i - 1][j])/ 4;
+                ddy = (uy[i][j + 1] - 2 * uy[i][j] + uy[i][j - 1])/ 4;
+                dt = 0.1* (ddx + ddy);
+                uybuf[i][j] = uy[i][j] +  dt;
+                pixels[4 *(WIDTH *j + i)] = (uint8_t)((uxbuf[i][j] + uybuf[i][j]) * 255);
              }
         }
-        for (size_t i = 1; i < WIDTH - 1; i++){
-            for (size_t j = 1; j < HEIGHT - 1; j++){
-                u[i][j] = ubuf[i][j];
+        for (size_t i = 0; i < WIDTH; i++){
+            uxbuf[i][0] = uxbuf[i][1];
+            uxbuf[i][HEIGHT - 1] =uxbuf[i][HEIGHT -2];
+            uybuf[i][0] = uybuf[i][1];
+            uybuf[i][HEIGHT - 1] =uybuf[i][HEIGHT -2];
+        }
+        for (size_t j = 0; j < HEIGHT; j++){
+        uxbuf[0][j] =uxbuf[1][j];
+        uxbuf[WIDTH - 1][j] = uxbuf[WIDTH -2][j];
+        uybuf[0][j] =uybuf[1][j];
+        uybuf[WIDTH - 1][j] = uybuf[WIDTH -2][j];
+    }
+        for (size_t i = 0; i < WIDTH ; i++){
+            for (size_t j = 0; j < HEIGHT ; j++){
+                ux[i][j] = uxbuf[i][j];
+                uy[i][j] = uybuf[i][j];
             }   
         }
         texture.update(pixels);
